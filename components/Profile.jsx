81ca15977cb19198f03e8f59ea183b4198ad3bdb
hanshhh <wenqi.zhan@mail.utoronto.ca>
2023-12-22 21:19:48 -0500

    Added tailwind css styling

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -2,7 +2,22 @@ import PromptCard from "./PromptCard";
 function Profile({name, desc, data, handleEdit, handleDelete}) {
     return (
         <section className="w-full">
-            <h1>{name} Profile</h1>
+            <h1 className="head_text text-left">
+                <span className="blue_gradient">{name} Profile </span>
+            </h1>
+            <p className="desc text-left">{desc}</p>
+            <div className="mt-10 prompt_layout">
+                {data.map((post) => (
+                    <PromptCard
+                        key={post?.id}
+                        post={post}
+                        handleEdit={() => {handleEdit && handleEdit(post)}}
+                        handleDelete={() => {handleDelete && handleDelete(post)}}
+                    />
+                ))}
+            </div>
+
+            
         </section>
     )
 }
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -4,6 +4,9 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import {usePathname, useRouter} from "next/navigation";
 function PromptCard({post, handleTagClick, handleEdit, handleDelete}) {
+    const {data: session} = useSession();
+    const pathName = usePathname();
+    const router = useRouter();
     const [copied, setCopied] = useState("");
     const handleCopy = () => {
         setCopied(post?.prompt);
@@ -42,6 +45,12 @@ function PromptCard({post, handleTagClick, handleEdit, handleDelete}) {
             <p className="font-inter text-sm blue_gradient cursor-pointer" onClick={() => {handleTagClick && handleTagClick(post?.tag)}}>
                 {post?.tag}
             </p>
+            {session?.user.id === post?.creator?._id && pathName === '/profile' && 
+            (<div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+                <p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}> Edit </p>
+                <p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleDelete}> Delete </p>
+
+            </div>)}
         </div>
     )
 }
